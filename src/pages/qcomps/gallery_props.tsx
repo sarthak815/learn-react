ad0ed1b264a9d4b1b5443b869fd78a4b217ce999
Sarthak Shastri <sarthak.shastri@gmail.com>
2025-01-25 17:36:19 -0500

    gallery_props update

--- src/pages/qcomps/gallery_props.tsx
+++ src/pages/qcomps/gallery_props.tsx
@@ -1,55 +1,61 @@
-export default function Gallery() {
+import { GalleryProps } from "@/types/gallery";
+function Scientists({ scientist, size = 70 }: GalleryProps) { 
   return (
     <div>
       <h1>Notable Scientists</h1>
       <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
+        <h2>
+          {scientist.name}
+        </h2>
         <img
           className="avatar"
-          src='https://i.imgur.com/szV5sdGs.jpg'
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
+          src={'https://i.imgur.com/' + scientist.imageId + '.jpg'}
+          alt={scientist.name}
+          width={size}
+          height={size}
         />
         <ul>
           <li>
             <b>Profession: </b>
-            physicist and chemist
+            {scientist.profession}
           </li>
           <li>
-            <b>Awards: 4 </b>
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (element)
-          </li>
-        </ul>
-      </section>
-      <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/YfeOqp2s.jpg'
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            geochemist
-          </li>
-          <li>
-            <b>Awards: 2 </b>
-            (Miyake Prize for geochemistry, Tanaka Prize)
+            <b>Awards: {scientist.awards.split(',').length} </b>
+            ({scientist.awards})
           </li>
           <li>
             <b>Discovered: </b>
-            a method for measuring carbon dioxide in seawater
+            {scientist.discovery}
           </li>
         </ul>
       </section>
+    </div>
+  );
+}
+
+export default function Gallery() {
+  return (
+    <div>
+      <Scientists
+        scientist={{
+          name: 'Katsuko Saruhashi',
+          imageId: 'YfeOqp2s',
+          profession: 'geochemist',
+          awards: 'Miyake Prize for geochemistry, Tanaka Prize',
+          discovery: 'a method for measuring carbon dioxide in seawater'
+        }}
+        size={150}
+      />
+      <Scientists
+        scientist={{
+          name: 'Maria Skłodowska-Curie',
+          imageId: 'szV5sdGs',
+          profession: 'physicist and chemist',
+          awards: 'Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal',
+          discovery: 'polonium (element)'
+        }}
+        size={150}
+      />  
     </div>
   );
 }
