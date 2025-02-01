43d4c62aa4a8ced4c0be012e0b87c76bf35d3df7
Sarthak Shastri <sarthak.shastri@gmail.com>
2025-02-01 17:26:32 -0500

    qcomps/updObjects

--- src/pages/qcomps/updObjectsForm.tsx
+++ src/pages/qcomps/updObjectsForm.tsx
@@ -13,7 +13,7 @@ export default function Scoreboard() {
   });
 
   function handlePlusClick() {
-    player.likescore++;
+    setPlayer({ ...player, likescore: player.likescore + 1 });
   }
 
   function handleFirstNameChange(e: { target: { value: string; }; }) {
@@ -24,7 +24,10 @@ export default function Scoreboard() {
   }
 
   function handleLastNameChange(e: { target: { value: string; }; }) {
-    player.lastName = e.target.value;
+    setPlayer({
+      ...player,
+      lastName: e.target.value,
+    });
   }
 
   return (
